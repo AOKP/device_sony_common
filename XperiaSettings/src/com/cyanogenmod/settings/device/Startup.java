c96a7c6b971a7d9ec2a976a66c6f1ec818e19fa2
Arnav Gupta <championswimmer@aokp.co>
2014-02-07 02:12:32 +0530

    XperiaSettings: complete rewrite of code
    Change-Id: I1d658e92911c8918629bb4187bd55b8ba152dc0e
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- XperiaSettings/src/com/cyanogenmod/settings/device/Startup.java
+++ XperiaSettings/src/com/cyanogenmod/settings/device/Startup.java
@@ -24,7 +24,6 @@ public class Startup extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
-        HapticFragmentActivity.restore(context);
         VibratorTuningPreference.restore(context);
     }
 }
