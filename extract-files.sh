CyanogenMod/android_device_sony_common

sony: common: fix extract file
Change-Id: I6496d6e60143e08f53be719fe6229c6bff7dd6e9

extract-files.sh
@@ -16,35 +16,29 @@ do
   esac
 done
 
-if [ "x$NC" != "x1" ]
-then
+if [ "x$NC" != "x1" ]; then
   rm -rf $BASE/*
 fi
 
 for FILE in `grep -v ^# ../$DEVICE/proprietary-files.txt | grep -v ^$ | sort`
 do
-  if [[ "$FILE" =~ ^obj:* ]]
-  then
-     FILE=`echo ${FILE##obj:}`
-  fi
-
   # Split the file from the destination (format is "file[:destination]")
   OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
   FILE=${PARSING_ARRAY[0]}
+  if [[ "$FILE" =~ ^-.* ]]; then
+    FILE=`echo $FILE | sed s/^-//`
+  fi
   DEST=${PARSING_ARRAY[1]}
-  if [ -z $DEST ]
-  then
-    DEST=$FILE
+  if [ -n "$DEST" ]; then
+    FILE=$DEST
   fi
   DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]
-  then
+  if [ ! -d $BASE/$DIR ]; then
     mkdir -p $BASE/$DIR
   fi
   adb pull /system/$FILE $BASE/$DEST
   # if file dot not exist try destination
-  if [ "$?" != "0" ]
-  then
+  if [ "$?" != "0" ]; then
     adb pull /system/$DEST $BASE/$DEST
   fi
 done

AOKP/device_sony_common

extract-files.sh
@@ -16,8 +16,7 @@ do
   esac
 done
 
-if [ "x$NC" != "x1" ]
-then
+if [ "x$NC" != "x1" ]; then
   rm -rf $BASE/*
 fi
 
@@ -27,19 +26,19 @@ do
   OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
   FILE=${PARSING_ARRAY[0]}
   DEST=${PARSING_ARRAY[1]}
-  if [ -z $DEST ]
-  then
+  if [[ "$FILE" =~ ^-.* ]]; then
+    FILE=`echo $FILE | sed s/^-//`
+  fi
+  if [ -z "$DEST" ]; then
     DEST=$FILE
   fi
   DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]
-  then
+  if [ ! -d $BASE/$DIR ]; then
     mkdir -p $BASE/$DIR
   fi
   adb pull /system/$FILE $BASE/$DEST
   # if file dot not exist try destination
-  if [ "$?" != "0" ]
-  then
+  if [ "$?" != "0" ]; then
     adb pull /system/$DEST $BASE/$DEST
   fi
 done